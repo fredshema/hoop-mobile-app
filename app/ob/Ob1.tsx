1e55ae74d8e1768c96f5a5bd972fa3fc4ffe8eb6
Fred Shema <shemafred3@gmail.com>
2024-04-10 12:53:20 +0200

    Refactor homepage

--- app/ob/Ob1.tsx
+++ app/ob/Ob1.tsx
@@ -1,36 +1,34 @@
+import Sizes from "@/constants/Sizes";
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 export default function Ob1() {
   return (
     <View style={styles.container}>
-      <Image
-        source={require("../../assets/auth/page1.png")}
-        style={styles.images}
-      />
+      <Image source={require("../../assets/auth/page1.png")} />
       <Text style={styles.title}>Welcome</Text>
       <Text style={styles.bodyMessage}>Find a best possible way to park</Text>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     alignContent: "center",
     justifyContent: "center",
-    backgroundColor: "#EAEAF3",
   },
   bodyMessage: {
     color: "#2D2D2D",
     opacity: 0.5,
-    fontSize: 16,
+    fontSize: Sizes.md2x,
     textAlign: "center",
+    maxWidth: 300,
   },
   title: {
     color: "#2D2D2D",
-    fontSize: 24,
+    fontSize: Sizes.xl,
     textAlign: "center",
-    marginTop: 20,
-  },
-  images: {
-    marginLeft: 50,
+    marginTop: Sizes.lg,
+    marginBottom: Sizes.sm,
   },
 });
+
--- app/ob/Ob2.tsx
+++ app/ob/Ob2.tsx
@@ -1,12 +1,10 @@
+import Sizes from "@/constants/Sizes";
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 export default function Ob2() {
   return (
     <View style={styles.container}>
-      <Image
-        source={require("../../assets/auth/page2.png")}
-        style={styles.images}
-      />
+      <Image source={require("../../assets/auth/page2.png")} />
       <Text style={styles.title}>Hollaaa</Text>
       <Text style={styles.bodyMessage}>
         Find the best possible parking space nearby your desired destination
@@ -14,25 +12,24 @@ export default function Ob2() {
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     alignContent: "center",
     justifyContent: "center",
-    backgroundColor: "#EAEAF3",
-  },
-  images: {
-    marginLeft: 50,
   },
   bodyMessage: {
     color: "#2D2D2D",
     opacity: 0.5,
-    fontSize: 16,
+    fontSize: Sizes.md2x,
     textAlign: "center",
+    maxWidth: 300,
   },
   title: {
     color: "#2D2D2D",
-    fontSize: 24,
+    fontSize: Sizes.xl,
     textAlign: "center",
-    marginTop: 20,
+    marginTop: Sizes.lg,
+    marginBottom: Sizes.sm,
   },
 });
