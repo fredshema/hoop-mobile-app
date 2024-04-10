fa9e0feabe8755d796b5da77b01c0c0eae46d4a0
Fred Shema <shemafred3@gmail.com>
2024-04-10 12:53:20 +0200

    Refactor homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,120 @@
-import { Link } from "expo-router";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Welcome from "./ob/Welcome";
-import Ob2 from "./ob/Ob2";
-import Ob3 from "./ob/Ob3";
-import Profile from "./profile/Profile";
-import Hom from "./Home/Hom";
-import Explore from "./Explore/Explore";
-import History from "./History/History";
-import Category from "./Category/Category";
-const Stack = createNativeStackNavigator();
+import Sizes from "@/constants/Sizes";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Ob1 from "./ob/Ob1";
 export default function Home() {
+  const insets = useSafeAreaInsets();
   return (
-  //   <NavigationContainer>
-  //   <Stack.Navigator>
-  //   <Stack.Screen name="ob1" component={Welcome} options={{headerShown:false}}/>
-  //   <Stack.Screen name="ob2" component={Ob2} options={{headerShown:false}} />
-  //   <Stack.Screen name="ob3" component={Ob3} options={{headerShown:false}}/>
-  //   </Stack.Navigator>
-  // </NavigationContainer>
-  <Category/>
- 
+    <View
+      style={[
+        styles.container,
+        { paddingTop: insets.top, paddingBottom: insets.bottom },
+      ]}
+    >
+      <Text style={styles.skipText}>Skip</Text>
+      <View style={styles.ob}>
+        <Ob1 />
+        <View style={styles.circles}>
+          <View style={[styles.circle, styles.activeCircle]}></View>
+          <View style={styles.circle}></View>
+          <View style={styles.circle}></View>
+        </View>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.btnEmail}>
+          <Text style={styles.textEmail}>Login with Email</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnPhone}>
+          <Text style={styles.textPhone}>Login with Phone</Text>
+        </TouchableOpacity>
+        <View style={styles.messageLink}>
+          <Text style={styles.message}>Don't have an account?</Text>
+          <Text style={styles.link}>Sign Up</Text>
+        </View>
+      </View>
+    </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: Sizes.lg,
+  },
+  skipText: {
+    textAlign: "right",
+    fontSize: Sizes.md3x,
+    fontFamily: "Avenir",
+    marginTop: Sizes.xl,
+  },
+  ob: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  circles: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  circle: {
+    width: 6,
+    height: 6,
+    borderRadius: 50,
+    backgroundColor: "#2D2D2D",
+    marginRight: 4,
+    opacity: 0.2,
+  },
+  activeCircle: {
+    backgroundColor: "#F43939",
+    opacity: 1,
+  },
+  buttons: {
+    alignItems: "center",
+  },
+  btnEmail: {
+    marginTop: 40,
+    width: 311,
+    height: 60,
+    borderRadius: 15,
+    backgroundColor: "#2D2D2D",
+  },
+  phoneIcon: {
+    marginRight: 8,
+  },
+  emailIcon: {
+    marginRight: 8,
+  },
+  textPhone: {
+    color: "#2D2D2D",
+    opacity: 0.7,
+    textAlign: "center",
+    marginTop: 15,
+    fontSize: 18,
+  },
+  message: {
+    color: "#2D2D2D",
+  },
+  textEmail: {
+    color: "#FFFFFF",
+    textAlign: "center",
+    marginTop: 15,
+    fontSize: 18,
+  },
+  btnPhone: {
+    width: 311,
+    height: 60,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 15,
+    marginTop: 20,
+  },
+  messageLink: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  link: {
+    color: "#F43939",
+    marginLeft: 10,
+  },
+});
--- app/ob/Ob3.tsx
+++ app/ob/Ob3.tsx
@@ -1,12 +1,10 @@
+import Sizes from "@/constants/Sizes";
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 export default function Ob3() {
   return (
     <View style={styles.container}>
-      <Image
-        source={require("../../assets/auth/page3.png")}
-        style={styles.images}
-      />
+      <Image source={require("../../assets/auth/page3.png")} />
       <Text style={styles.title}>Find Parking</Text>
       <Text style={styles.bodyMessage}>
         Find your perfect parking space wherever and whenever you need
@@ -18,21 +16,19 @@ const styles = StyleSheet.create({
   container: {
     alignContent: "center",
     justifyContent: "center",
-    backgroundColor: "#EAEAF3",
   },
   bodyMessage: {
     color: "#2D2D2D",
     opacity: 0.5,
-    fontSize: 16,
+    fontSize: Sizes.md2x,
     textAlign: "center",
+    maxWidth: 300,
   },
   title: {
     color: "#2D2D2D",
-    fontSize: 24,
+    fontSize: Sizes.xl,
     textAlign: "center",
-    marginTop: 20,
-  },
-  images: {
-    marginLeft: 50,
+    marginTop: Sizes.lg,
+    marginBottom: Sizes.sm,
   },
 });
