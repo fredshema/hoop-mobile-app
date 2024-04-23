4d3ef5cdeb3a24e9076edf59697c19d7980ca72b
Fred Shema <shemafred3@gmail.com>
2024-04-23 14:10:32 +0200

    feat: add registration implementation

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,3 +1,4 @@
+import client from "@/Utils/AppwriteClient";
 import { Link, Text } from "@/components/Themed";
 import { PrimaryButton } from "@/components/ThemedButton";
 import { PasswordInput, PhoneInput, TextInput } from "@/components/ThemedInput";
@@ -5,14 +6,45 @@ import Colors from "@/constants/Colors";
 import Sizes from "@/constants/Sizes";
 import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { ImageBackground, StyleSheet, View } from "react-native";
+import { Account, ID } from "react-native-appwrite/src";
 
 export default function Register() {
   const bgImage = require("../../assets/auth/pattern.png");
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordAuth, setPasswordAuth] = useState("");
+  const [phone, setPhone] = useState("");
+
+  async function register(
+    name: string,
+    email: string,
+    password: string,
+    passwordAuth: string,
+    phone: string
+  ) {
+    if (password !== passwordAuth) {
+      console.error("Passwords do not match");
+      return;
+    }
+
+    const account = new Account(client);
+    try {
+      await account.create(ID.unique(), email, password, name);
+      await account.updatePrefs(phone);
+      await account.createEmailSession(email, password);
+      router.push("/home/");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <StatusBar style="inverted" />
+      <StatusBar style="dark" />
       <View style={styles.header}>
         <ImageBackground source={bgImage} style={styles.bg}>
           <Text style={styles.title}>Let's Start!!</Text>
@@ -21,16 +53,40 @@ export default function Register() {
       <View style={styles.form}>
         <View style={{ flex: 1 }}>
           <View>
-            <TextInput placeholder="Email" inputMode="email" />
-            <PasswordInput placeholder="Password" secureTextEntry />
-            <PasswordInput placeholder="Password Authentication" />
-            <PhoneInput placeholder="Phone number" returnKeyType="done" />
+            <TextInput placeholder="Name" value={name} onChangeText={setName} />
+            <TextInput
+              placeholder="Email"
+              inputMode="email"
+              value={email}
+              onChangeText={setEmail}
+            />
+            <PasswordInput
+              placeholder="Password"
+              secureTextEntry
+              value={password}
+              onChangeText={setPassword}
+            />
+            <PasswordInput
+              placeholder="Password Authentication"
+              secureTextEntry
+              value={passwordAuth}
+              onChangeText={setPasswordAuth}
+            />
+            <PhoneInput
+              placeholder="Phone number"
+              returnKeyType="done"
+              value={phone}
+              onChangeText={setPhone}
+            />
           </View>
         </View>
         <View style={styles.footer}>
-          <PrimaryButton label="Register" onPress={()=>{
-            router.push("/home/")
-          }} />
+          <PrimaryButton
+            label="Register"
+            onPress={() => {
+              register(name, email, password, passwordAuth, phone);
+            }}
+          />
           <Text style={{ color: Colors.light.muted }}>
             Have an account? <Link href="/(auth)/login">Sign In</Link>
           </Text>
