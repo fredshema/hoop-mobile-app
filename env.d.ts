dd04cdd9bed90bbda59cae7e40e61274e4140548
latiah <kimtifah2@gmail.com>
2024-04-26 16:10:28 +0200

    fix:fixed conflicts due to the arrangment of variables in a file

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 declare module '@env' {
     export const APPWRITE_PROJECT_ID: string;
-    export const DATABASE_ID:string;
-    export const COLLECTION_ID:string;
     export const APPWRITE_DATABASE_ID: string;
     export const APPWRITE_NOTIFICATIONS_COLLECTION_ID: string;
+    export const DATABASE_ID:string;
+    export const COLLECTION_ID:string;
 }
